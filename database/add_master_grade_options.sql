2e8562f44fe6fd1bbffab35126e914655c59836c
Tameshwari <tameshwarisahu961@gmail.com>
2026-06-11 11:21:03 +0530

    Add full grade master seed data

---
+++ database/add_master_grade_options.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO master_grades (grade_name)
+VALUES
+  ('RS1'), ('RS2'), ('RS3'), ('RS4'), ('RS5'), ('RS6'), ('RS7'), ('RS8'), ('RS9'), ('RS10'),
+  ('JO'),
+  ('M1'), ('M2'), ('M3'), ('M4'), ('M5'), ('M6'), ('M7'), ('M8'), ('M9'), ('M10'), ('M11');
--- database/schema.sql
+++ database/schema.sql
@@ -137,7 +137,10 @@ CREATE TABLE IF NOT EXISTS tour_supporting_documents (
 );
 
 INSERT IGNORE INTO master_grades (grade_name)
-VALUES ('RS1'), ('RS2'), ('RS3'), ('RS4'), ('RS5'), ('RS6'), ('RS7'), ('RS8');
+VALUES
+  ('RS1'), ('RS2'), ('RS3'), ('RS4'), ('RS5'), ('RS6'), ('RS7'), ('RS8'), ('RS9'), ('RS10'),
+  ('JO'),
+  ('M1'), ('M2'), ('M3'), ('M4'), ('M5'), ('M6'), ('M7'), ('M8'), ('M9'), ('M10'), ('M11');
 
 INSERT IGNORE INTO master_departments (department_name)
 VALUES ('C & IT'), ('Civil'), ('Electrical'), ('Mechanical'), ('Finance'), ('HR');
